4eece40dbe453f49bfc09e6a03d55735ad261419
carl-chua <carlchua0701@gmail.com>
2024-06-08 02:30:48 +0800

    feat: add material ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,26 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import CssBaseline from '@mui/material/CssBaseline';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
-
 export const metadata: Metadata = {
-  title: 'Split Smart',
-  description: 'Split your bills smartly',
+	title: 'Split Smart',
+	description: 'Split your bills smartly',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang='en'>
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<meta name='viewport' content='initial-scale=1, width=device-width' />
+			<CssBaseline />
+			<body>{children}</body>
+		</html>
+	);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,45 @@
 'use client';
 
-import { ThemeProvider } from '@material-tailwind/react';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import RestoreIcon from '@mui/icons-material/Restore';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import Paper from '@mui/material/Paper';
 import { SessionProvider } from 'next-auth/react';
+import { useState } from 'react';
 
 import { Navbar } from '@/components/Navbar';
 
+export function SimpleBottomNavigation() {
+	const [value, setValue] = useState(0);
+
+	return (
+		<Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+			<BottomNavigation
+				showLabels
+				value={value}
+				onChange={(event, newValue) => {
+					setValue(newValue);
+				}}
+			>
+				<BottomNavigationAction label='Recents' icon={<RestoreIcon />} />
+				<BottomNavigationAction label='Favorites' icon={<FavoriteIcon />} />
+				<BottomNavigationAction label='Nearby' icon={<LocationOnIcon />} />
+			</BottomNavigation>
+		</Paper>
+	);
+}
+
 export default function Home() {
-  return (
-    <ThemeProvider>
-      <SessionProvider>
-        <Navbar />
-        <main className='flex min-h-screen flex-col items-center p-10'>
-          <div className=''>Home</div>
-        </main>
-      </SessionProvider>
-    </ThemeProvider>
-  );
+	return (
+		<SessionProvider>
+			<main className='flex min-h-screen flex-col items-center'>
+				<Navbar />
+				<div className=''>Above navbar is using material tailwind</div>
+				<div className=''>Bottom navigation is using material ui</div>
+				<SimpleBottomNavigation />
+			</main>
+		</SessionProvider>
+	);
 }
